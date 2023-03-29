34f34d7cb0852afa27622fb00cccdf0ead209dd2
Daniel Borges <86573375+Danielpnz@users.noreply.github.com>
2023-03-28 21:54:19 -0300

    Aula 02
    Essa foi a segunda aula da imersão...
    Consegui alocar todas as imagens para a pasta "saida" usando o colocando o paste na linha 41 :
    
                var geradora = new FabricaDeFigurinhas();

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -22,10 +24,20 @@ public class App {
         List<Map<String, String>> listaDeFilmes = jsonParser.parse(body); 
 
         // EXIBIR E MANIPULAR OS DADOS
+
+        var geradora = new FabricaDeFigurinhas();
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL (urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            
+
+            System.out.println(filme.get(titulo));
             System.out.println();
 
         }
